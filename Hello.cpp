899af2768a6f69a0df1c2cf3011d385bcbf4f831
pjoys <phoebesayao@gmail.com>
2024-01-30 13:33:48 -0800

    created Hello.cpp
    init commit

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+/*
+*
+*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
