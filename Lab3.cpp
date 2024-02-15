d2d3f9ff9520708c8602aabdc9a0af8ef60dcde0
pjoys <phoebesayao@gmail.com>
2024-02-15 13:03:23 -0800

    changes

---
+++ Lab3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    int x = 0;
+    int y = 5;
+
+    while(x<y){
+        printf("%i\n",x);
+        x++;
+    }
+    printf("Looped through 5 times");
+}
