f87f89468b99d38795b4e7db6ed9df4a823cd33b
pjoys <phoebesayao@gmail.com>
2024-02-26 23:14:03 -0800

    Update Lab4_func.cpp

--- Lab4/Lab4_func.cpp
+++ Lab4/Lab4_func.cpp
@@ -12,7 +12,81 @@ using namespace std;
 
 int sumOfThree(int x, int y, int z)
 {
-    
+    // initialize 3 int parameters
+    int x1 = x;
+    int y1 = y;
+    int z1 = z;
+
+    // calculate sum of 3 int
+    int sumAnswer = x1 + y1 + z1;
+    return sumAnswer;
+}
+
+/** division
+ * @brief returns x / y
+ * 
+ * @param x : (double)
+ * @param y : (double)
+ * @return double
+*/
+
+double division(double x, double y)
+{
+    double x1 = x;
+    double y1 = y;
+
+    double quotient = x / y;
+    return quotient;
+}
+
+/** isGreatherThan
+* @brief returns whether x is greater than y
+*
+* @param x : (double)
+* @param y : (int)
+* @return true : x is greater than y
+* @return false : x is less than or equal to y
+*/
+
+bool isGreaterThan(double x, int y)
+{
+    double x1 = x;
+    int y1 = y;
+
+
+}
+
+/** select
+* @brief output the statements based on func_type
+* func_type : output
+* "sum" : "The sum is "
+* "division" : "The quotient is "
+* "comparison" : "The first value being greater than the second value is "
+* All other phrases: "Invalid function type\n"
+* @param func_type : (string)
+* @return string
+*/
+
+string select(string func_type) {
+    string out = "";
+    if(func_type == "sum") {
+        printf("The sum is "); 
+    }
+    else {
+        if(func_type == "division") {
+            out = "The quotient is ";
+        }
+        else {
+            if(func_type == "comparison") {
+                out = "The first value being greater than the second value is ";
+            }
+            else{
+                out = "Invalid function type\n";
+            }
+        }
+    }
+
+    return out;
 }
 
 /** myPrint
@@ -38,18 +112,18 @@ int main()
     // call sumOfThree
     int sum_out = sumOfThree(x1, x2, x3);
     // call myPrint
-    myPrint("sum", sum_out)
+    myPrint("sum", sum_out);
 
     // initialize 2 double parameters
     double dividend = 12345.1;
     double divisor = 2345.6;
 
     // call division
-    double dov_out = division( dividend, divisor);
+    double div_out = division(dividend, divisor);
     myPrint("division", div_out);
 
     // compare div_out and sum_out
     bool is_greater_out = isGreaterThan (div_out, sum_out);
-    myPrint("comparison", is_greater_than);
+    myPrint("comparison", is_greater_out);
     return 0;
 }
